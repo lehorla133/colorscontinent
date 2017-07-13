cb2d26a382479653c428103375ed7bdb2a401fcf
Lauren Bill <lauren.bill@heig-vd.ch>
2017-07-13 17:52:37 +0200

    first commit hello

---
+++ site/snippets/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="<?= site()->language() ? site()->language()->code() : 'en' ?>">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+  <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
+  <meta name="description" content="<?= $site->description()->html() ?>">
+
+  <?= css('assets/bootstrap/css/bootstrap-theme.min.css') ?>
+  <?= css('assets/bootstrap/css/bootstrap.min.css') ?>
+
+  <?= js('assets/js/jquery-3.2.1.min.js') ?>
+  <?= js('assets/bootstrap/js/bootstrap.min.js') ?>
+
+</head>
+<body>
+
+  <header class="header wrap wide" role="banner">
+    <div class="grid">
+
+      <div class="branding column">
+        <a href="<?= url() ?>" rel="home"><?= $site->title()->html() ?></a>
+      </div>
+
+      <?php snippet('menu') ?>
+
+    </div>
+  </header>
